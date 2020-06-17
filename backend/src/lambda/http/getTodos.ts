01e2e7812391e1a0543884d1f4a4cfb41fd65c47
Laurent Graebner <laurent.graebner@gmail.com>
2020-06-18 00:13:56 +0200

    fix: return map with items

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -4,19 +4,25 @@ import * as AuthUtils from '../../auth/utils'
 import { getAllTodoItems } from '../../businessLogic/todos'
 import *  as middy from 'middy'
 import { cors } from 'middy/middlewares'
+import { createLogger } from '../../utils/logger'
+
+const logger = createLogger('getTodos')
 
 export const handler= middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
+  logger.info('caller event ', { event: event})
 
   const token: string = AuthUtils.getTokenFromApiGatewayEvent(event)
-  const items = await getAllTodoItems(token)
+  const items  = await getAllTodoItems(token)
 
   return {
       statusCode: 200,
       headers: {
           'Access-Control-Allow-Origin': '*'
       },
-      body: JSON.stringify(items)
+      body: JSON.stringify({
+          items: items
+      })
   }
 
 })
@@ -26,4 +32,4 @@ handler.use(
     cors({
         credentials: true
     })
-  )
+)
