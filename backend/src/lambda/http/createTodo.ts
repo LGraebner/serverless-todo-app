b2286e7c6bfffe0c940252ebf7de5d31b711a371
Laurent Graebner <laurent.graebner@gmail.com>
2020-06-18 00:14:08 +0200

    Add more logging

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -5,9 +5,15 @@ import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 import { createNewTodoItem } from '../../businessLogic/todos'
 import *  as middy from 'middy'
 import { cors } from 'middy/middlewares'
+import { createLogger } from '../../utils/logger'
+
+
+const logger = createLogger('createTodo')
 
 export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     // TODO: Implement creating a new TODO item
+  logger.info('caller event ', { event: event})
+
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
   const token: string = AuthUtils.getTokenFromApiGatewayEvent(event)
   const item = await createNewTodoItem(newTodo, token)
--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -3,8 +3,13 @@ import 'source-map-support/register'
 import { generateUploadUrl } from '../../businessLogic/todos'
 import *  as middy from 'middy'
 import { cors } from 'middy/middlewares'
+import { createLogger } from '../../utils/logger'
+
+const logger = createLogger('generateUploadUrls')
 
 export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  logger.info('caller event ', { event: event})
+
   const todoId = event.pathParameters.todoId
   const uploadUrl =  await generateUploadUrl(todoId)
 
